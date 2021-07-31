faceffcca7064df7e400313ca42cc9a220c2239e
godofinvesting <sungjun94@gmail.com>
2021-07-30 20:01:50 -0400

    improve website feature and design

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 let cp = document.getElementById('contact-point');
 let toggler = document.querySelectorAll('.navbar-toggler')
 let contact = document.querySelector('#contact');
+let resume = document.getElementById('resume-point')
+let resumesection = document.getElementById('resume')
+
 cp.addEventListener('click', () => {
-  console.log(contact.classList)
   if (contact.classList.contains('displaynone')) {
     setTimeout(() => {contact.classList.remove('displaynone')}, 400);
   } 
@@ -12,4 +14,14 @@ toggler.forEach(element => element.addEventListener('click', () => {
   if (!contact.classList.contains('displaynone')) {
     contact.classList.add('displaynone');
   }
+  if (!resumesection.classList.contains('displaynone1')) {
+    resumesection.classList.add('displaynone1');
+  }
 }))
+
+resume.addEventListener('click', () => {
+  if (resumesection.classList.contains('displaynone1')) {
+    setTimeout(() => {resumesection.classList.remove('displaynone1')}, 400);
+  }
+})
+
