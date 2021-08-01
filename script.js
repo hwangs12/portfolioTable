94089437c66a7f491ae7c30fa412b5a8ac94053b
hwangs12 <sungjun94@gmail.com>
2021-08-01 00:31:28 -0400

    styling website

--- script.js
+++ script.js
@@ -1,27 +1,53 @@
-let cp = document.getElementById('contact-point');
-let toggler = document.querySelectorAll('.navbar-toggler')
-let contact = document.querySelector('#contact');
-let resume = document.getElementById('resume-point')
-let resumesection = document.getElementById('resume')
+let cp = document.getElementById("contact-point");
+let toggler = document.querySelectorAll(".navbar-toggler");
+let contact = document.querySelector("#contact");
+let resume = document.getElementById("resume-point");
+let resumesection = document.getElementById("resume");
+let carousel = document.getElementById("carouselExampleInterval");
 
-cp.addEventListener('click', () => {
-  if (contact.classList.contains('displaynone')) {
-    setTimeout(() => {contact.classList.remove('displaynone')}, 400);
-  } 
-})
+cp.addEventListener("click", () => {
+	if (contact.classList.contains("displaynone")) {
+		setTimeout(() => {
+			contact.classList.remove("displaynone");
+		}, 400);
+	}
+});
 
-toggler.forEach(element => element.addEventListener('click', () => {
-  if (!contact.classList.contains('displaynone')) {
-    contact.classList.add('displaynone');
-  }
-  if (!resumesection.classList.contains('displaynone1')) {
-    resumesection.classList.add('displaynone1');
-  }
-}))
+toggler.forEach((element) =>
+	element.addEventListener("click", () => {
+		if (!contact.classList.contains("displaynone")) {
+			contact.classList.add("displaynone");
+		}
+		if (!resumesection.classList.contains("displaynone1")) {
+			resumesection.classList.add("displaynone1");
+		}
+	})
+);
+toggler.forEach((element) =>
+	element.addEventListener("click", () => {
+		if (!contact.classList.contains("displaynone")) {
+			contact.classList.add("displaynone");
+		}
+		if (!resumesection.classList.contains("displaynone1")) {
+			resumesection.classList.add("displaynone1");
+		}
+	})
+);
 
-resume.addEventListener('click', () => {
-  if (resumesection.classList.contains('displaynone1')) {
-    setTimeout(() => {resumesection.classList.remove('displaynone1')}, 400);
-  }
-})
+resume.addEventListener("click", () => {
+	if (resumesection.classList.contains("displaynone1")) {
+		setTimeout(() => {
+			resumesection.classList.remove("displaynone1");
+		}, 400);
+	}
+});
 
+toggler.forEach((element) =>
+	element.addEventListener("click", () => {
+		if (carousel.style.display !== "none") {
+			carousel.style.display = "none";
+		} else {
+			carousel.style.display = "unset";
+		}
+	})
+);
